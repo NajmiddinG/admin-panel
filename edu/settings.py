23fc4a407ebc5a5ce8326f54319e8665ff267e29
NajmiddinG <najmiddindev@gmail.com>
2022-07-06 15:54:06 +0500

    s3 added

--- edu/settings.py
+++ edu/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'edu_app',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,18 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+# S3
+AWS_ACCESS_KEY_ID='AKIARAD2L6LL22QUZYUG'
+AWS_SECRET_ACCESS_KEY='fIyqa2daiKES3rgzlJLbnrz2u5IYV6t7X+7m37tp'
+AWS_STORAGE_BUCKET_NAME='adminpanelforstudy'
+AWS_URL='https://YOUR-BUCKET-NAME.s3.amazonaws.com/'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
